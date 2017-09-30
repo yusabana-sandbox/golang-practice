be0bc12b9003d7948169a6a07cb691d7b9ac0f8f
Yuji Takaesu <taka.esu.user@gmail.com>
2017-09-30 16:35:09 +0900

    不要なファイル等の整理

---
+++ gotour/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+// "github.com/yusabana-sandbox/golang-practice/newton"
+
+func main() {
+	fmt.Println("User")
+	// fmt.Println(
+	// 	newton.Sqrt(8.0),
+	// 	math.Sqrt(8.0),
+	// )
+}
---
+++ gotour/newton.go
@@ -0,0 +1,10 @@
+package newton
+
+// Sqrt ok
+func Sqrt(x float64) float64 {
+	z := 2.0
+	for i := 0; i < 10000000; i++ {
+		z -= (z*z - x) / (2 * z)
+	}
+	return z
+}
