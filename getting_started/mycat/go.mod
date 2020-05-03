c7e25c06d08ca3137de7363e8a0347c5a9d04fb7
Yuji Takaesu <taka.esu.user@gmail.com>
2020-05-03 10:56:02 +0900

    feat: Add mycat command

---
+++ getting_started/mycat/go.mod
@@ -0,0 +1,3 @@
+module github.com/yusabana-sandbox/golang-practice/getting_started/mycat
+
+go 1.14
--- getting_started/mycat/main.go
+++ getting_started/mycat/main.go
@@ -1,6 +1,45 @@
 package main
 
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+)
+
+var optN = flag.Bool("n", false, "行番号")
+
 func main() {
 	println("CATコマンドです")
+
+	flag.Parse()
+	for _, fn := range flag.Args() {
+		err := readFile(fn, *optN)
+		if err != nil { log.Fatal(err) }
+	}
 }
 
+func readFile(fn string, optN bool) (error) {
+	sf, err := os.Open(fn)
+	if err != nil { return err }
+	defer sf.Close() // fileハンドラを閉じるのを予約
+
+	num := 1
+	scanner := bufio.NewScanner(sf)
+	for scanner.Scan() {
+		text := scanner.Text()
+
+		if optN {
+			fmt.Fprintln(os.Stdout, num, ":", text)
+		} else {
+			fmt.Fprintln(os.Stdout, text)
+		}
+
+		num++
+	}
+	if err := scanner.Err(); err != nil { return err }
+
+
+	return nil
+}
