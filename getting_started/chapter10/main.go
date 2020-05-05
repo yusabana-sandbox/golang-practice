6978849aed719db85c6e60c8e75da56547c42c53
Yuji Takaesu <taka.esu.user@gmail.com>
2020-05-05 23:31:58 +0900

    feat: use html/template for output html

--- getting_started/chapter10/main.go
+++ getting_started/chapter10/main.go
@@ -3,47 +3,67 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"html/template"
 	"log"
 	"math/rand"
 	"net/http"
 	"time"
 )
 
-type MyHandler struct{
+type MyHandler struct {
 	http.Handler
 }
+
 func (mh MyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "HELLO, HANDLERで定義")
 }
 
+var tmpl = template.Must(template.New("sign").
+	Parse("<html><body>{{.Name}}さんの運勢は「<strong>{{.Omikuji}}</strong>」です</body></html>"))
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "HELLO, HANDLER_FUNCで定義")
+	name := r.FormValue("p")
+
+	data := struct {
+		Name    string
+		Omikuji string
+	}{
+		Name:    name,
+		Omikuji: omikuji(),
+	}
+	tmpl.Execute(w, data)
 }
 
 func handlerOmikuji(w http.ResponseWriter, r *http.Request) {
-	num := rand.Intn(6) +1
+	result := omikuji()
+	fmt.Fprint(w, "おみくじ結果は"+result)
+}
 
+func omikuji() string {
+	// rand 0-5の6パターンなので+1してswitchでサイコロ飲めのようにおみくじを決める
+	num := rand.Intn(6) + 1
+	var r string
 	switch num {
 	case 6:
-		fmt.Fprint(w, "おみくじ結果は大吉")
-	case 5,4:
-		fmt.Fprint(w, "おみくじ結果は中吉")
-	case 3,2:
-		fmt.Fprint(w, "おみくじ結果は吉")
+		r = "大吉"
+	case 5, 4:
+		r = "中吉"
+	case 3, 2:
+		r = "吉"
 	case 1:
-		fmt.Fprint(w, "おみくじ結果は凶")
+		r = "凶"
 	}
+	return r
 }
 
 func handleMyJson(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
 	v := struct {
 		Name string `json:"N"` // 個々のタグで指定したキーでjsonに変換される
-		Msg string `json:"msg"`
+		Msg  string `json:"msg"`
 	}{
 		Name: "Hoge",
-		Msg: "hello",
+		Msg:  "hello",
 	}
 
 	// encodeするとレスポンスがJSONになる?!
@@ -52,27 +72,10 @@ func handleMyJson(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func handleReq(w http.ResponseWriter, r *http.Request) {
-	val := r.FormValue("p")
-
-	w.Header().Set("Content-Type", "application/json; charset=utf-8")
-	v := struct {
-		Msg string `json:"msg"`
-	}{
-		Msg: fmt.Sprintf("%sさんの運勢は「大吉」です", val),
-	}
-
-	if err := json.NewEncoder(w).Encode(v); err != nil {
-		log.Println("Erro:", err)
-	}
-}
-
-func init() {
+func main() {
 	// 乱数の初期化
 	rand.Seed(time.Now().UnixNano())
-}
 
-func main() {
 	http.HandleFunc("/", handlerFunc)
 
 	handler := MyHandler{}
@@ -83,12 +86,6 @@ func main() {
 
 	// JSONサンプル
 	http.HandleFunc("/myjson", handleMyJson)
-	http.HandleFunc("/req", handleReq)
-
 
 	http.ListenAndServe(":8080", nil)
 }
-
-
-
-
