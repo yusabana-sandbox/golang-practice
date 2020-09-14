e6a38268da30c5375ba412f2621d6afbe646ec8f
Yuji Takaesu <taka.esu.user@gmail.com>
2020-09-14 15:52:51 +0900

    feat: multiple return value

--- had-experience-of-programming/cmd/hoge/main.go
+++ had-experience-of-programming/cmd/hoge/main.go
@@ -4,4 +4,13 @@ import "fmt"
 
 func main() {
 	fmt.Println("HOGEですよねー")
+
+	a, b, _ := multipleReturn()
+
+	fmt.Println(a, b)
+}
+
+
+func multipleReturn() (string, int, int) {
+	return "hoge", 3, 4
 }
