bed73965bf021e276dd9ff76be093f33ef5d4552
Yuji Takaesu <taka.esu.user@gmail.com>
2020-09-14 23:01:08 +0900

    feat: array, slice, range and arguments of variable length

--- had-experience-of-programming/chapter2/chapter2.go
+++ had-experience-of-programming/chapter2/chapter2.go
@@ -7,7 +7,6 @@ import (
 )
 
 func Do() {
-
 	fmt.Println("HOGEですよねー")
 
 	a, b, _ := multipleReturn()
@@ -27,12 +26,33 @@ func Do() {
 	fmt.Println(div2)
 
 	// 関数リテラル
-	func(i, j int) { fmt.Println(i + j) }(3,5)
+	func(i, j int) { fmt.Println(i + j) }(3, 5)
 	// 関数リテラルの変数代入
 	var sum func(i, j int) = func(i, j int) {
 		fmt.Println(i + j)
 	}
 	sum(2, 4)
+
+	// 配列(スライスではない)
+	arr()
+
+	// スライス
+	slice()
+
+	// Range
+	rng()
+
+	// 値の切り出し
+	// string, 配列, スライスから値を部分的に取り出せる
+	s := []int{0, 1, 2, 3, 4, 5}
+	fmt.Println("", s[2:4])      // [2 3]
+	fmt.Println("", s[0:len(s)]) // [0 1 2 3 4 5]
+	fmt.Println("", s[:3])       // [0 1 2]
+	fmt.Println("", s[3:])       // [3 4 5]
+	fmt.Println("", s[:])        // [0 1 2 3 4 5]
+
+	// sumFuncは可変長引数
+	fmt.Println(sumFunc(1, 2, 3, 4, 5))
 }
 
 func multipleReturn() (string, int, int) {
@@ -56,5 +76,49 @@ func divNamed(i, j int) (result int, err error) {
 		return
 	}
 	result = i / j
+	return
+}
+
+// 配列
+func arr() {
+	fmt.Println("配列について")
+	// どちらも同じ型
+	arr1 := [4]string{"A", "B", "c", "d"}
+	// この書き方は可変長のものではない(スライスではない)
+	arr2 := [...]string{"A", "B", "c", "d"}
+
+	sampleArr(arr1)
+	sampleArr(arr2)
+}
+
+// 型引数に配列自体が渡されているのでコピーが渡されている
+func sampleArr(arr [4]string) {
+	fmt.Println(arr)
+}
+
+func slice() {
+	fmt.Println("スライスについて")
+	s := []string{"a", "b", "C", "D"}
+	fmt.Println(s)
+	s = append(s, "PIYO")
+	fmt.Println(s)
+}
+
+func rng() {
+	fmt.Println("レンジについて")
+	arr1 := [4]string{"a", "b", "c", "d"}
+
+	for i, s := range arr1 {
+		// i 添字, s 文字列
+		fmt.Println(i, s)
+	}
+}
+
+// 可変長引数
+func sumFunc(nums ...int) (result int) {
+	// numsは var nums []int と同じ感じ
+	for _, n := range nums {
+		result += n
+	}
 	return
 }
