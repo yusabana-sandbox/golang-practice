21163b655dd70e8265b54a88a5a498c5916b6db9
Yuji Takaesu <taka.esu.user@gmail.com>
2020-09-14 22:14:11 +0900

    feat: literal of functions

--- had-experience-of-programming/cmd/hoge/main.go
+++ had-experience-of-programming/cmd/hoge/main.go
@@ -18,8 +18,21 @@ func main() {
 		log.Fatal(err)
 	}
 	fmt.Println(div1)
-}
 
+	div2, err2 := divNamed(6, 2)
+	if err2 != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(div2)
+
+	// 関数リテラル
+	func(i, j int) { fmt.Println(i + j) }(3,5)
+	// 関数リテラルの変数代入
+	var sum func(i, j int) = func(i, j int) {
+		fmt.Println(i + j)
+	}
+	sum(2, 4)
+}
 
 func multipleReturn() (string, int, int) {
 	return "hoge", 3, 4
@@ -36,8 +49,8 @@ func div(i, j int) (int, error) {
 }
 
 // 名前付き戻り値
-func divNamed(i,j int) (result int, err error) {
-	if j==0 {
+func divNamed(i, j int) (result int, err error) {
+	if j == 0 {
 		err = errors.New("divided by zero of divNamed")
 		return
 	}
