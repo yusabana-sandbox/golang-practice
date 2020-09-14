3f349fe238423222580e0a8812d1e9a3f3a96ec5
Yuji Takaesu <taka.esu.user@gmail.com>
2020-09-14 21:59:02 +0900

    feat: error samples

--- had-experience-of-programming/cmd/hoge/main.go
+++ had-experience-of-programming/cmd/hoge/main.go
@@ -1,15 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"log"
+)
 
 func main() {
 	fmt.Println("HOGEですよねー")
 
 	a, b, _ := multipleReturn()
 	fmt.Println(a, b)
+
+	div1, err := div(4, 0)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(div1)
 }
 
 
 func multipleReturn() (string, int, int) {
 	return "hoge", 3, 4
+}
+
+
+func div(i float32, j int) (int, error) {
+	if j == 0 {
+		// 自作のエラーを返す
+		return 0, errors.New("divided by zero")
+	}
+
+	return int(i) / j, nil
 }
