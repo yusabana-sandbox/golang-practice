c744d2ac1e420ed55ae2bff66619503063ce0e3c
Yuji Takaesu <taka.esu.user@gmail.com>
2020-09-15 20:04:34 +0900

    feat: Init chapter4

---
+++ had-experience-of-programming/chapter4/chapter4.go
@@ -0,0 +1,7 @@
+package chapter4
+
+import "fmt"
+
+func Do() {
+	fmt.Println("AAAAAA")
+}
--- had-experience-of-programming/cmd/hoge/main.go
+++ had-experience-of-programming/cmd/hoge/main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"github.com/yusabana-sandbox/golang-practice/had-experience-of-programming/chapter2"
 	"github.com/yusabana-sandbox/golang-practice/had-experience-of-programming/chapter3"
+	"github.com/yusabana-sandbox/golang-practice/had-experience-of-programming/chapter4"
 )
 
 func main() {
 	chapter2.Do()
 	chapter3.Do()
+	chapter4.Do()
 }
