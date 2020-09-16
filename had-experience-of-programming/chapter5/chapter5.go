cf1779fdc80927ad30140c21716a82ded80d0a15
Yuji Takaesu <taka.esu.user@gmail.com>
2020-09-17 03:23:30 +0900

    feat: advanced for channel

--- had-experience-of-programming/chapter5/chapter5.go
+++ had-experience-of-programming/chapter5/chapter5.go
@@ -11,6 +11,59 @@ func Do() {
 	fmt.Println("chapter5はじまるよーーーー")
 
 	doGoroutin()
+
+	doGoroutinChannel()
+}
+
+func doGoroutinChannel() {
+	urls := []string{
+		"http://example.com",
+		"http://example.net",
+		"http://example.org",
+	}
+
+	// 直接チャネルを処理する書き方
+	//// channelを作る
+	//statusChan := make(chan string)
+	//
+	//for _, url := range urls {
+	//	go func(url string) {
+	//		res, err := http.Get(url)
+	//		if err != nil {
+	//			log.Fatal(err)
+	//		}
+	//		defer res.Body.Close()
+	//
+	//		statusChan <- fmt.Sprintf("%s %s", res.Status, url)
+	//	}(url)
+	//}
+	// 関数に切り出して返り値として該当のチャネルを受け取る書き方
+	statusChan := getStatus(urls)
+
+	// <-statusChanとチャネルを読み込みする際処理がブロックされるのでwithGroupのような待ち合わせは不要
+	// 3個のリクエストを投げているから3回チャネルからのデータを待ち合わせておけばOK
+	for i := 0; i < len(urls); i++ {
+		fmt.Println(<-statusChan)
+	}
+}
+
+// チャネルを返す関数
+// <-chan string のような戻り値の型を指定し、読み出し専用のチャネルにすることで不用意にchannelへの書き込みを出来ないようにする
+func getStatus(urls []string) <-chan string {
+	// channelを作る
+	statusChan := make(chan string)
+
+	for _, url := range urls {
+		go func(url string) {
+			res, err := http.Get(url)
+			if err != nil {
+				log.Fatal(err)
+			}
+			defer res.Body.Close()
+			statusChan <- fmt.Sprintf("%s %s", res.Status, url)
+		}(url)
+	}
+	return statusChan
 }
 
 func doGoroutin() {
@@ -30,8 +83,8 @@ func doGoroutin() {
 			if err != nil {
 				log.Fatal(err)
 			}
-
 			defer res.Body.Close()
+
 			fmt.Println(url, res.Status)
 
 			// waitGroupから削除
