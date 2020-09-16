538c1d94c236b7421c4ae369cc90ffc2c4998829
Yuji Takaesu <taka.esu.user@gmail.com>
2020-09-17 00:47:43 +0900

    feat: samples net/http GET

--- had-experience-of-programming/chapter4/net_http.go
+++ had-experience-of-programming/chapter4/net_http.go
@@ -3,9 +3,11 @@ package chapter4
 import (
 	"encoding/json"
 	"fmt"
+	"html/template"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 type WebPerson struct {
@@ -22,11 +24,27 @@ func DoNetHttp() {
 	http.ListenAndServe(":8000", nil)
 }
 
-// 以下のコマンドで実行する
-// $ curl -vvvv http://localhost:8000/persons -d '{"id":1, "name": "ほげふが"}'
+// テンプレートのコンパイル
+// ParseFiles()は戻り地にerrを返すが、
+// t, err := template.ParseFiles("index.html")
+// Must()を一緒に用いるとエラー位に戻り地じゃなくてパニックを発生させることができる
+// そもそもテンプレートがないとかコンパイルできないとかは実行時エラーと言うよりはおかしい状態なのでpanicにするのがいい
+// 一度コンパイルが通ることを確認したテンプレートであれば毎回エラー処理を書く必要性は低いのでMust()を使うのがいい
+var t = template.Must(template.ParseFiles("index.html"))
+
+/*
+以下のコマンドを実行してPOSTするとファイルが生成される
+POST
+$ curl -vvvv http://localhost:8000/persons -d '{"id":1, "name": "ほげふが"}'
+
+以下のコマンドはGETしてファイルに書き込まれた情報を取得する
+$ curl -vvvv 'http://localhost:8000/persons/?id=1'
+ */
 func PersonHandler(w http.ResponseWriter, r *http.Request) {
 	// 処理の最後にBodyを閉じる
 	defer r.Body.Close()
+
+	fmt.Println("aaaa")
 
 	if r.Method == "POST" {
 		// リクエストボディをJSONに変換
@@ -53,5 +71,24 @@ func PersonHandler(w http.ResponseWriter, r *http.Request) {
 
 		w.WriteHeader(http.StatusCreated)
 		fmt.Fprint(w, "Created!!")
+	} else if r.Method == "GET" {
+		// パラメータを取得(Atoi()を使ってstringからintに変換する)
+		id, err := strconv.Atoi(r.URL.Query().Get("id"))
+		if err != nil {
+			log.Fatal(err)
+		}
+		filename := fmt.Sprintf("%d.txt", id)
+		b, err := ioutil.ReadFile(filename)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		person := Person{
+			Id:   id,
+			Name: string(b),
+		}
+
+		// templateに対して実行する。それをHTTPのレスポンスとして返す
+		t.Execute(w, person)
 	}
 }
