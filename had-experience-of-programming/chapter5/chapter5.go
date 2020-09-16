87a30abb501b1cb0e3308b0e7cc54e4c0ff69351
Yuji Takaesu <taka.esu.user@gmail.com>
2020-09-17 03:04:03 +0900

    feat: goroutine samples

--- had-experience-of-programming/chapter5/chapter5.go
+++ had-experience-of-programming/chapter5/chapter5.go
@@ -1,7 +1,44 @@
 package chapter5
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"sync"
+)
 
 func Do() {
 	fmt.Println("chapter5はじまるよーーーー")
+
+	doGoroutin()
+}
+
+func doGoroutin() {
+	wait := new(sync.WaitGroup)
+	urls := []string{
+		"http://example.com",
+		"http://example.net",
+		"http://example.org",
+	}
+
+	for _, url := range urls {
+		// waitGroupに追加
+		wait.Add(1)
+
+		go func(url string) {
+			res, err := http.Get(url)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			defer res.Body.Close()
+			fmt.Println(url, res.Status)
+
+			// waitGroupから削除
+			wait.Done()
+		}(url)
+	}
+
+	// 全部が完了するまで待ち合わせる
+	wait.Wait()
 }
