1aa12ccf8e3df145ca063d7312e6495b0b2bc312
Yuji Takaesu <taka.esu.user@gmail.com>
2020-09-28 00:13:41 +0900

    fix: remove bracket from if clause

--- had-experience-of-programming/fuga/errors.go
+++ had-experience-of-programming/fuga/errors.go
@@ -9,9 +9,8 @@ import (
 func RunErrors() {
 	// 存在しないファイルを指定必ず以下のエラーに入るはず
 	file, err := os.Open("__AAAAAAA.txt")
-
 	// errorインターフェースにはError()というメソッドがある
-	if (err != nil) {
+	if err != nil {
 		fmt.Println("エラーよ")
 		fmt.Println(err.Error()) // open __AAAAAAA.txt: no such file or directory
 		return
@@ -23,9 +22,11 @@ func RunErrors() {
 	// エラーではないときの処理
 	fmt.Println("ファイルが開いた")
 
-	// 独自のエラーを返す場合
-	e := test()
-	fmt.Println(e.Error())
+	//独自のエラーを返す場合
+	// エラーの場合 errorだけを返す場合はこのように簡潔に書ける
+	if e := test(); e != nil {
+		fmt.Println(e.Error())
+	}
 }
 
 func test() error {
