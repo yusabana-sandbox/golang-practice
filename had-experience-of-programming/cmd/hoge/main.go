05e9a9dedef30c199e14d59344568461ce7c1259
Yuji Takaesu <taka.esu.user@gmail.com>
2020-09-29 14:02:19 +0900

    feat: Goroutin of chapter5

--- had-experience-of-programming/cmd/hoge/main.go
+++ had-experience-of-programming/cmd/hoge/main.go
@@ -1,17 +1,15 @@
 package main
 
-import (
-	"github.com/yusabana-sandbox/golang-practice/had-experience-of-programming/chapter2"
-)
+import "github.com/yusabana-sandbox/golang-practice/had-experience-of-programming/chapter5"
 
 // go build cmd/hoge
 // でビルドするとhogeというコマンドができる
 func main() {
-	chapter2.Do()
+	//chapter2.Do()
 	//chapter3.Do()
 	//chapter4.Do()
 	//// serverとして起動するので適宜利用するときだけOnにする
 	////chapter4.DoNetHttp()
 	//
-	//chapter5.Do()
+	chapter5.Do()
 }
